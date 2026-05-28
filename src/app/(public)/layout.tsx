913f27f08bb002da1a128315ae281f754e7cc0f0
Lilfyloo <lombard.dam@gmail.com>
2026-05-28 14:52:49 +0200

    feat: Adding login Error gestion

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,29 +1,8 @@
 import { Banner } from "@/components/common/banner/banner";
 import { Sidebar } from "@/components/common/sidebar/sidebar";
 import type { ReactNode } from "react";
-import type { Metadata } from "next";
-import { Fraunces, Plus_Jakarta_Sans } from "next/font/google";
-import "../globals.css";
-import { cn } from "@/lib/utils";
 
-const frauncesHeading = Fraunces({
-  subsets: ["latin"],
-  variable: "--font-frances",
-});
-
-const plusJakarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  variable: "--font-plus-jakarta",
-});
-
-export const metadata: Metadata = {
-  title: "GamerChallenges",
-  description:
-    // TODO Change métadata
-    "Participate in epic gaming challenges and compete on the leaderboard",
-};
-
-export default function RootLayout({
+export default function PublicLayout({
   children,
 }: Readonly<{
   children: ReactNode;
--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -13,12 +13,23 @@ export function useLogin() {
     setLoading(true)
 
     const formData = new FormData(e.currentTarget)
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if(!email || !password) {
+      setError("Veuillez remplir tous les champs")
+      setLoading(false)
+      return
+    }
+
+    if(!email.includes("@")) {
+      setError("Email invalide")
+      setLoading(false)
+      return
+    }
 
     try {
-      const data = await login(
-        formData.get("email") as string,
-        formData.get("password") as string
-      )
+      const data = await login(email, password)
       return data
     } catch (err) {
       const message = err instanceof Error ? err.message : "Erreur inconnue"
