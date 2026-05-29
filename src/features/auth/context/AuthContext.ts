f41b396c416002489d9b995b9ac4d5c51d61be30
Tens-gtb <tno.lacassagne@gmail.com>
2026-05-29 17:04:46 +0200

    Fix: Try fixed error 400 register page

--- src/features/auth/context/AuthContext.ts
+++ src/features/auth/context/AuthContext.ts
@@ -1,27 +1,27 @@
-"use client"
+"use client";
 
 import React, { createContext, useState, useEffect, ReactNode } from "react";
 import { User } from "@/features/auth/types/auth.type";
 import "dotenv/config";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 type AuthContextType = {
-  user: User | null
-  isAuthenticated: boolean
-  login: (user: User) => void
-  logout: () => void
-}
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (user: User) => void;
+  logout: () => void;
+};
 
 export const AuthContext = createContext<AuthContextType>({
   user: null,
   isAuthenticated: false,
   login: () => {},
   logout: () => {},
-})
+});
 
-export function AuthProvider({ children }: { children: ReactNode }){
-  const [user, setUser] = useState<User | null>(null)
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     fetch(`${API_URL}/auth/me`, {
@@ -29,24 +29,24 @@ export function AuthProvider({ children }: { children: ReactNode }){
     })
       .then((res) => (res.ok ? res.json() : null))
       .then((me) => me && setUser(me))
-      .catch(() => setUser(null))
-  }, [])
+      .catch(() => setUser(null));
+  }, []);
 
   function login(user: User) {
-    setUser(user)
+    setUser(user);
   }
 
   async function logout() {
     await fetch(`${API_URL}/auth/logout`, {
       method: "POST",
       credentials: "include",
-    })
-    setUser(null)
+    });
+    setUser(null);
   }
 
-  return React.createElement (
+  return React.createElement(
     AuthContext.Provider,
-    { value: { user, isAuthenticated: !!user, login, logout}},
-    children
-  )
+    { value: { user, isAuthenticated: !!user, login, logout } },
+    children,
+  );
 }
