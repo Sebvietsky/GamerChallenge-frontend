5c1968b2525b8bc938e25d2ab2a649f2fb9fe92f
Tens-gtb <tno.lacassagne@gmail.com>
2026-06-09 17:02:50 +0200

    Feat: Create Participation => logic => page update => cardChallenge on Participate

--- src/features/types/createSchema.ts
+++ src/features/types/createSchema.ts
@@ -1,12 +1,18 @@
-export interface createChallengePayload {
-  title: string,
-  igdbId: number,
-  description: string,
-  demo?: string,
-  goals?: string,
-  difficultyId: number,
-  challengeCategoryId: number,
-  hints?: string,
-  status?: string,
-  closesAt?: Date,
+export interface CreateChallengePayload {
+  title: string;
+  igdbId: number;
+  description: string;
+  demo?: string;
+  goals?: string;
+  difficultyId: number;
+  challengeCategoryId: number;
+  hints?: string;
+  status?: string;
+  closesAt?: Date;
+}
+
+export interface CreateParticipationPayload {
+  title: string;
+  description: string;
+  video: string;
 }
