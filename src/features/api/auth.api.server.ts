71036aefc2a23a2dcede93391acf9d1faacb5680
Lilfyloo <lombard.dam@gmail.com>
2026-06-12 09:19:18 +0200

    COnsole log on getMe

--- src/features/api/auth.api.server.ts
+++ src/features/api/auth.api.server.ts
@@ -5,11 +5,12 @@ import { API_BASE_URL } from "@/lib/api";
 export async function getMe(): Promise<User | null> {
   try {
     const headers = await getServerAuthHeaders();
+    console.log("cookies envoyés:", headers)
     const response = await fetch(`${API_BASE_URL}/auth/me`, {
       method: "GET",
       headers,
     })
-
+    console.log("status getMe:", response.status)
     if (!response.ok) return null
 
     return response.json()
