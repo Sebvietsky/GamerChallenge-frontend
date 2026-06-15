ccdb2755c1348b853402213bd8434d5bd753afd4
Lilfyloo <lombard.dam@gmail.com>
2026-06-15 10:14:13 +0200

    fix: chalenge Div missing

--- src/app/(public)/participations/trends/page.tsx
+++ src/app/(public)/participations/trends/page.tsx
@@ -3,9 +3,7 @@ import { ParticipationsPageClient } from "@/components/participations/participat
 export default async function ParticipationsPage() {
   return (
     <div className="space-y-8 p-8">
-      <div className="flex-1 lg:ml-64 pt-20">
-        <h1 className="text-3xl font-bold">Les Participations à la une</h1>
-      </div>
+      <h1 className="text-3xl font-bold">Les Participations à la une</h1>
       <ParticipationsPageClient />
     </div>
   );
