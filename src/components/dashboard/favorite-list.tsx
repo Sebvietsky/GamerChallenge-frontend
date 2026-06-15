809f07596ff0c32abaaaabf9879482c6f3b8f481
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2026-06-15 10:48:54 +0200

    fix: delete Image/next import from favorite list

--- src/components/dashboard/favorite-list.tsx
+++ src/components/dashboard/favorite-list.tsx
@@ -9,7 +9,6 @@ import type { ChallengeItem } from "@/features/types/challenge.type";
 
 // STYLE
 import { homeStyles as styles } from "@/styles/global.styles";
-import Image from "next/image";
 
 type FavoriteChallengeListProps = {
   favoriteChallenges: ChallengeItem[];
